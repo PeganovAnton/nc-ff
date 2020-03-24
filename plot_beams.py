a48bf2c64e3866bce1a619082206eb6b479d49f5
PeganovAnton <peganoff2@mail.ru>
2020-03-24 11:16:33 +0300

    Add possibility of passing config to plot drawing script

--- plot_beams.py
+++ plot_beams.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 
 import matplotlib.pyplot as plt
@@ -39,46 +40,64 @@ def parse_args():
         '-b',
         help="The name of the dimension which is varied in 1 beam."
     )
-    return parser.parse_args()
+    parser.add_argument(
+        '--config',
+        help="Path to JSON config with plotting parameters. If parameters "
+             "were passed to script directly they have higher priority "
+             "compared with config."
+    )
+    args = parser.parse_args()
+    with open(args.config, 'r') as f:
+        config = json.load(f)
+    config['data_path'] = args.data_path
+    if args.save_dir is not None:
+        config['save_dir'] = args.save_dir
+    if args.plot_dim is not None:
+        config['plot_dim'] = args.plot_dim
+    if args.color_dim is not None:
+        config['color_dim'] = args.color_dim
+    if args.beam_dim is not None:
+        config['beam_dim'] = args.beam_dim
+    return config
 
 
 def get_color(idx):
     return COLORS[idx]
 
 
-def plot_beam(da, color, beam_dim, color_coord):
-    mean = da.reduce(np.mean, beam_dim)
+def plot_beam(da, config, color, color_coord):
+    mean = da.reduce(np.mean, config['beam_dim'])
     plt.plot(
         da.coords['values'],
         mean,
         lw=1,
         color=color,
-        label=beam_dim + ': ' + str(color_coord)
+        label=config['color_dim'] + ': ' + str(color_coord.data)
     )
-    for beam_coord in da.coords[beam_dim]:
+    for beam_coord in da.coords[config['beam_dim']]:
         plt.plot(
             da.coords['values'],
-            da.sel(**{beam_dim: beam_coord}),
-            lw=0.2,
+            da.sel(**{config['beam_dim']: beam_coord}),
+            lw=0.3,
             color=color,
         )
 
 
-def draw_beams_plot(da, save_file, plot_dim, color_dim, beam_dim):
+def draw_beams_plot(da, save_file, config, plot_coord):
     fig, ax = plt.subplots()
-    for i, color_coord in enumerate(da.coords[color_dim]):
+    for i, color_coord in enumerate(da.coords[config['color_dim']]):
         color = get_color(i)
         plot_beam(
             da.sel(
-                **{color_dim: color_coord}),
+                **{config['color_dim']: color_coord}),
+            config,
             color,
-            beam_dim,
-            color_coord.data
+            color_coord
         )
     ax.grid()
     ax.set_xlabel('step')
     ax.set_xscale('log')
-    ax.set_ylabel(plot_dim)
+    ax.set_ylabel(config['ylabels'][str(plot_coord.data)])
     ax.legend()
     plt.tight_layout()
     dir_, file = os.path.split(save_file)
@@ -86,27 +105,23 @@ def draw_beams_plot(da, save_file, plot_dim, color_dim, beam_dim):
     plt.savefig(save_file, dpi=900)
 
 
-def draw_beams_plots(da, save_dir, plot_dim, color_dim, beam_dim):
-    for plot_coord in da.coords[plot_dim]:
+def draw_beams_plots(da, config):
+    for plot_coord in da.coords[config['plot_dim']]:
         print(plot_coord.data)
         draw_beams_plot(
-            da.sel(**{plot_dim: plot_coord}),
-            os.path.join(save_dir, str(plot_coord.data) + '.png'),
-            plot_dim,
-            color_dim,
-            beam_dim
+            da.sel(**{config['plot_dim']: plot_coord}),
+            os.path.join(config['save_dir'], str(plot_coord.data) + '.png'),
+            config,
+            plot_coord
         )
 
 
 def main():
-    args = parse_args()
-    da = xa.load_dataarray(args.data_path)
+    config = parse_args()
+    da = xa.load_dataarray(config['data_path'])
     draw_beams_plots(
         da,
-        args.save_dir,
-        args.plot_dim,
-        args.color_dim,
-        args.beam_dim
+        config
     )
 
 
