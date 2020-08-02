a8d7be39ac6477a8f7a34bbe9a6c829abc2644fe
PeganovAnton <peganoff2@mail.ru>
2020-08-02 17:29:31 +0300

    back up

--- commands.sh
+++ commands.sh
@@ -47,3 +47,20 @@ cd ~/nc-ff/results/ff
 python3 ../../results_to_data_array.py adam4/ \
     "^\./lr([0-9\.\-e+]*)/([0-9])/\w*/(?:(?:valid/(\w*).*)|(?:(\w*).pickle))$" \
     -t float int str -d learning_rate launch_number metric step
+
+
+# ff unbalanced
+# Сбор результатов эксперимента adam lr 0.001 при варьировании энтропии.
+cd ~/nc-ff/results/ff
+python3 ../../results_to_data_array.py unbalanced_ds_entropy/adam4_lr0.0001 \
+    "^\./en_([0-9\.]*)/([0-9])/\w*/(?:(?:valid/(\w*).*)|(?:(\w*).pickle))$" \
+    -t float int str -d entropy launch_number metric step
+python3 ../../results_to_data_array.py unbalanced_ds_entropy/en_1.829_adam4/ \
+    "^\./lr([0-9\.]*)/([0-9])/\w*/(?:(?:valid/(\w*).*)|(?:(\w*).pickle))$" \
+    -t float int str -d learning_rate launch_number metric step
+python3 ../../results_to_data_array.py unbalanced_ds_entropy/sgd4_lr0.001 \
+    "^\./en_([0-9\.]*)/([0-9])/\w*/(?:(?:valid/(\w*).*)|(?:(\w*).pickle))$" \
+    -t float int str -d entropy launch_number metric step
+python3 ../../results_to_data_array.py unbalanced_ds_entropy/en_1.829_sgd4/ \
+    "^\./lr([0-9\.]*)/([0-9])/\w*/(?:(?:valid/(\w*).*)|(?:(\w*).pickle))$" \
+    -t float int str -d learning_rate launch_number metric step
